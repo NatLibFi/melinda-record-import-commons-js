b3dbbdaf8c5738e5b257d703d0ca74f68cdaeef1
Olli Pajuluoma <olli.pajuluoma@leadin.fi>
2018-04-26 14:23:26 +0300

    Enums and HTTP-codes that are used at least on API and Controller

---
+++ utils/HttpCodes.js
@@ -0,0 +1,49 @@
+/**
+*
+* @licstart  The following is the entire license notice for the JavaScript code in this file. 
+*
+* API microservice of Melinda record batch import system
+*
+* Copyright (C) 2018 University Of Helsinki (The National Library Of Finland)
+*
+* This file is part of melinda-record-import-api
+*
+* melinda-record-import-api program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* melinda-record-import-api is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @licend  The above is the entire license notice
+* for the JavaScript code in this file.
+*
+*/
+
+/* eslint-disable no-unused-vars */
+
+module.exports = {
+    OK: 200,
+    Created: 201,
+    Accepted: 202,
+    NoContent: 204,
+    Malformed: 400,
+    BadRequest: 400,
+    Unauthorized: 401,
+    Forbidden: 403,
+    NotFound: 404,
+    MethodNotAllowed: 405,
+    Conflict: 409,
+    Teapot: 418,
+    ValidationError: 422,
+    InternalServerError: 500,
+    NotImplemented: 501,
+    BadGateway: 502,
+    ServiceUnavailable: 503
+};
