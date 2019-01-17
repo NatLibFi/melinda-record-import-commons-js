0204d2d0fb922875d606fb31717e5001d3de720d
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2019-01-17 14:53:36 +0200

    Add transformer CLI

--- src/transformer.js
+++ src/transformer.js
@@ -29,6 +29,9 @@
 /* Not sure why this is needed only in this module... */
 /* eslint-disable import/default */
 
+import fs from 'fs';
+import yargs from 'yargs';
+import ora from 'ora';
 import amqp from 'amqplib';
 import {checkEnv as checkEnvShared, createLogger} from './common';
 import {BLOB_STATE} from './constants';
@@ -104,4 +107,47 @@ export async function runValidation(validateFunc, records, fix = false) {
 		failed: !result.valid,
 		messages: result.report
 	}));
+}
+
+export async function cli(name, transformCallback, validateCallback) {
+	const args = yargs
+		.scriptName(name)
+		.command('$0 <file>', '', yargs => {
+			yargs
+				.positional('file', {type: 'string', describe: 'File to transform'})
+				.option('v', {alias: 'validate', default: false, type: 'boolean', describe: 'Validate records'})
+				.option('f', {alias: 'fix', default: false, type: 'boolean', describe: 'Validate & fix records'})
+				.option('r', {alias: 'recordsOnly', default: false, type: 'boolean', describe: 'Write only record data to output (Invalid records are excluded)'});
+		})
+		.parse();
+
+	if (!fs.existsSync(args.file)) {
+		console.error(`File ${args.file} does not exist`);
+		process.exit(-1);
+	}
+
+	const spinner = ora('Transforming records').start();
+	const records = await transformCallback(fs.createReadStream(args.file));
+
+	if (args.validate || args.fix) {
+		spinner.succeed();
+		spinner.start('Validating records');
+
+		const results = await validateCallback(records, args.fix);
+		const invalidCount = results.filter(r => r.failed).length;
+		const validCount = results.length - invalidCount;
+		spinner.succeed(`Validating records (Valid: ${validCount}, invalid: ${invalidCount})`);
+
+		if (args.recordsOnly) {
+			console.error(`Excluding ${results.filter(r => r.failed).length} failed records`);
+			console.log(JSON.stringify(results.filter(r => !r.failed).map(r => r.record.toObject()), undefined, 2));
+		} else {
+			console.log(JSON.stringify(results.map(r => {
+				return Object.assign(r, {record: r.record.toObject()});
+			}), undefined, 2));
+		}
+	} else {
+		spinner.succeed();
+		console.log(JSON.stringify(records.map(r => r.toObject()), undefined, 2));
+	}
 }
