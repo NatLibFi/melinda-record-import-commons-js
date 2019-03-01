ce29ac3cfe5ba7320bbe3d8b32f44aaf67fed20c
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2019-03-01 12:51:28 +0200

    Add health check command for Docker

---
+++ src/health-check.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+/**
+*
+* @licstart  The following is the entire license notice for the JavaScript code in this file.
+*
+* Shared modules for microservices of Melinda record batch import system
+*
+* Copyright (C) 2018-2019 University Of Helsinki (The National Library Of Finland)
+*
+* This file is part of melinda-record-import-commons
+*
+* melinda-record-import-commons program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* melinda-record-import-commons is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @licend  The above is the entire license notice
+* for the JavaScript code in this file.
+*
+*/
+
+const fetch = require('node-fetch');
+
+run();
+
+async function run() {
+	try {
+		const port = process.argv[2] ? Number.parseInt(process.argv[2], 10) : 8080;
+		const response = await fetch(`http://localhost:${port}/healthz`);
+		process.exit(response.status === 200 ? 0 : 1);
+	} catch (err) {
+		process.exit(1);
+	}
+}
