771bf932ec1f3708c133d77239a9178f24abee5b
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2019-10-04 09:12:56 +0300

    some debug

--- src/transformer/transformer.js
+++ src/transformer/transformer.js
@@ -87,27 +87,41 @@ export default async function (transformCallback) {
 					.on('record', async payload => {
 						pendingPromises.push(recordEvent(payload));
 
-						async function recordEvent(payload) {
+						function recordEvent(payload) {
 							// Logger.log('debug', 'Record failed: ' + payload.failed);
 							payload.timeStamp = moment();
 
-							if (payload.failed) {
-								hasFailed = true;
-								await ApiClient.transformedRecord({
-									id: BLOB_ID,
-									record: payload.record
-								});
-							} else {
-								await ApiClient.transformedRecord({
-									id: BLOB_ID
-								});
+							return Promise.all(sendRecodToQueue(channel, payload), updateBlob(payload));
+
+							async function sendRecodToQueue(payload) {
+								if ((!ABORT_ON_INVALID_RECORDS || (ABORT_ON_INVALID_RECORDS && !hasFailed))) {
+									try {
+										channel.assertQueue(BLOB_ID, {durable: true});
+										const message = Buffer.from(JSON.stringify(payload.record));
+										await channel.sendToQueue(BLOB_ID, message, {persistent: true, messageId: uuid()});
+									} catch (err) {
+										logger.log('error', `Error while sending record to queue: ${err instanceof Error ? err.stack : err}`);
+									}
+									// Logger.log('debug', `Record sent to queue as profile: ${PROFILE_ID}`);
+								}
 							}
 
-							if ((!ABORT_ON_INVALID_RECORDS || (ABORT_ON_INVALID_RECORDS && !hasFailed))) {
-								channel.assertQueue(BLOB_ID, {durable: true});
-								const message = Buffer.from(JSON.stringify(payload.record));
-								await channel.sendToQueue(BLOB_ID, message, {persistent: true, messageId: uuid()});
-								// Logger.log('debug', `Record sent to queue as profile: ${PROFILE_ID}`);
+							async function updateBlob(payload) {
+								try {
+									if (payload.failed) {
+										hasFailed = true;
+										await ApiClient.transformedRecord({
+											id: BLOB_ID,
+											record: payload.record
+										});
+									} else {
+										await ApiClient.transformedRecord({
+											id: BLOB_ID
+										});
+									}
+								} catch (err) {
+									logger.log('error', `Error while updating blob: ${err instanceof Error ? err.stack : err}`);
+								}
 							}
 						}
 					});
