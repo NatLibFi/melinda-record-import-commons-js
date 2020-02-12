88c1cd2e1d154ac376245c70268be57a0df6dabd
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2020-02-12 15:25:24 +0200

    Fail on errors properly (#26)

--- src/transformer/cli.js
+++ src/transformer/cli.js
@@ -74,7 +74,7 @@ export default async ({name, yargsOptions = [], callback}) => {
 				resolve();
 			})
 			.on('error', err => {
-				logger.log('error', err);
+				spinner.fail();
 				reject(err);
 			})
 			.on('record', async payload => {
