5d5dca783f1bb5e63aa3f3c7bdfffd2b60edf07b
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2020-04-01 09:54:35 +0300

    Move logging blob at the top

--- src/transformer/transformer.js
+++ src/transformer/transformer.js
@@ -59,10 +59,12 @@ export default async function (transformCallback) {
 		let connection;
 		let channel;
 
+		logger.log('info', `Starting transformation for blob ${BLOB_ID}`);
+		
 		const ApiClient = createApiClient({url: API_URL, username: API_USERNAME, password: API_PASSWORD, userAgent: API_CLIENT_USER_AGENT});
 		const {readStream} = await ApiClient.getBlobContent({id: BLOB_ID});
 
-		logger.log('info', `Starting transformation for blob ${BLOB_ID}`);
+
 
 		try {
 			let hasFailed = false;
