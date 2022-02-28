6808b1a0e9d33fabee4a6d9f838d1d4394d4e796
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2022-02-28 09:07:41 +0200

    fix lint

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,14 @@
 import moment from 'moment';
 import createDebugLogger from 'debug';
 
+const debug = createDebugLogger('@natlibfi/melinda-record-import-commons:utils');
+
+
 export function isOfflinePeriod(importOfflinePeriod) {
+  if (importOfflinePeriod === undefined) {
+    return false;
+  }
+
   const {startHour, lengthHours} = importOfflinePeriod;
   const now = moment();
 
@@ -21,8 +28,8 @@ export function isOfflinePeriod(importOfflinePeriod) {
   }
 }
 
-export async function getNextBlobId(riApiClient, {profileId, state}) {
-  logger.log('debug', `Checking blobs for ${profileId} in ${state}`);
+export async function getNextBlobId(riApiClient, {profileId, state, importOfflinePeriod}) {
+  debug(`Checking blobs for ${profileId} in ${state}`);
   let result = ''; // eslint-disable-line functional/no-let
 
   try {
@@ -36,39 +43,39 @@ export async function getNextBlobId(riApiClient, {profileId, state}) {
 
     // Returns false or blob id
     return result;
-  } catch (err) {
-    logger.error(err);
+  } catch (error) {
+    debug(error);
   }
-}
 
-function processBlobs({client, query, processCallback, messageCallback, updateState = false, filter = () => true}) {
-  return new Promise((resolve, reject) => {
-    const wantedBlobs = [];
+  function processBlobs({client, query, processCallback, messageCallback, updateState = false, filter = () => true}) {
+    return new Promise((resolve, reject) => {
+      const wantedBlobs = [];
 
-    const debug = createDebugLogger('@natlibfi/melinda-record-import-commons:processBlobs');
-    const emitter = client.getBlobs(query);
+      const debug = createDebugLogger('@natlibfi/melinda-record-import-commons:processBlobs');
+      const emitter = client.getBlobs(query);
 
-    emitter
-      .on('error', reject)
-      .on('blobs', blobs => {
-        const filteredBlobs = blobs.filter(filter);
-        filteredBlobs.forEach(blob => wantedBlobs.push(blob)); // eslint-disable-line functional/immutable-data
-      })
-      .on('end', () => {
-        if (messageCallback) { // eslint-disable-line functional/no-conditional-statement
-          debug(messageCallback(wantedBlobs.length));
-        }
-        const result = processCallback(wantedBlobs, updateState);
+      emitter
+        .on('error', reject)
+        .on('blobs', blobs => {
+          const filteredBlobs = blobs.filter(filter);
+          filteredBlobs.forEach(blob => wantedBlobs.push(blob)); // eslint-disable-line functional/immutable-data
+        })
+        .on('end', () => {
+          if (messageCallback) { // eslint-disable-line functional/no-conditional-statement
+            debug(messageCallback(wantedBlobs.length));
+          }
+          const result = processCallback(wantedBlobs, updateState);
 
-        resolve(result);
-      });
-  });
+          resolve(result);
+        });
+    });
+  }
 
   function processCallback(blobs) {
     const [blob] = blobs;
 
-    if (blob === undefined || isOfflinePeriod(config.importOfflinePeriod)) {
-      logger.debug('No blobs or offline period');
+    if (blob === undefined || isOfflinePeriod(importOfflinePeriod)) {
+      debug('No blobs or offline period');
       return false;
     }
 
