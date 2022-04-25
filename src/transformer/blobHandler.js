04892abf44af62c0edbf2abe292053a70b7ddbd5
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2022-04-25 10:55:07 +0300

    fix TRANSFORMED

--- src/transformer/blobHandler.js
+++ src/transformer/blobHandler.js
@@ -1,5 +1,5 @@
 import {v4 as uuid} from 'uuid';
-import {BLOB_STATE} from '../common';
+import {BLOB_STATE} from '../constants';
 import {closeAmqpResources} from '../utils';
 import createDebugLogger from 'debug';
 
